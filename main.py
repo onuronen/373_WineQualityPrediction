58f90b16048b7a2ab5ed67b45256bd773bbf9bd2
henrywu00 <wu1461@purdue.edu>
2021-04-15 18:54:22 -0700

    added comments for svm

--- main.py
+++ main.py
@@ -14,18 +14,22 @@ print(Y)
 print(Y.shape)
 
 
-#svm
+# svm
 import random
 import svm
 import svmpredict
 
+# number of total samples
 n = len(X)
 
+# subset sizes that we are using
 subset = [500, 600, 700, 800, 900, 1000]
 err = []
 
+# train and test the modle on each subset
 for i in range(len(subset)):
 
+    # randomly sample from dataset
     index = random.sample(list(range(n)), subset[i])
     X_sub = []
     Y_sub = []
@@ -33,23 +37,27 @@ for i in range(len(subset)):
         X_sub.append(X[index[j]])
         Y_sub.append(Y[index[j]])
 
+    # split subset into equal halves for training and testing
     X_train = X_sub[len(X_sub)//2:]
     Y_train = Y_sub[len(Y_sub)//2:]
 
     X_test = X_sub[:len(X_sub)//2]
     Y_test = Y_sub[:len(Y_sub)//2]
 
+    # train the model and test the model
     clf = svm.run(X_train,Y_train)
     Y_predict = clf.predict(X_test)
 
     s = 0
 
+    # calculate the prediction error
     for i in range(len(Y_predict)):
         if Y_predict[i] == Y_test[i]:
             s += 1
 
     err.append(s / len(Y_predict))
 
+# final array of prediction errors
 print(err)
 
 
--- svm.py
+++ svm.py
@@ -1,6 +1,9 @@
 from sklearn import svm
 import numpy as np
 
+# Input: matrix X of features, with n rows, d columns
+# vector y of labels, with n rows
+# Output: Trained svm model object
 def run(X,y):
     clf = svm.NuSVC(gamma="auto")
     clf.fit(X,y)
