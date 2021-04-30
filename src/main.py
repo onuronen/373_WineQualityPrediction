56d6862abe840f04a4bac8703076cc09ef158f15
onuronen <oonen@purdue.edu>
2021-04-30 18:50:56 -0400

    final

--- src/main.py
+++ src/main.py
@@ -28,7 +28,7 @@ for i in subset:
     Y_sub = Y[0:i]
 
     #split training and testing half-half
-    X_train, X_test, y_train, y_test = train_test_split(X_sub, Y_sub, test_size = 0.5, random_state = 42)
+    X_train, X_test, y_train, y_test = train_test_split(X_sub, Y_sub, test_size = 0.5, random_state = 44)
     (training_length, dd) = np.shape(X_train)
     (test_length, features) = np.shape(X_test)
 
